a36aaa03da7552c5f6fd1d2aed36ba6307718e64
kcook98765 <56806582+kcook98765@users.noreply.github.com>
2022-10-24 12:37:05 -0700

    Allow for multiple calls via parameters and set specified properties

--- default.py
+++ default.py
@@ -2,17 +2,45 @@ import xbmc, xbmcgui, xbmcaddon
 import sys
 import urllib.request
 import json
+from urllib.parse import parse_qs
 
 win = xbmcgui.Window(10000)
 win.setProperty("libreTranslate_text", '')
 
 def main():
+
+    params = {}
+    for arg in sys.argv:
+        if arg == 'default.py':
+            pass
+        elif '=' in arg:
+            arg_split = arg.split('=', 1)
+            if arg_split[0] and arg_split[1]:
+                key, value = arg_split
+                params.setdefault(key, value)
+        else:
+            params.setdefault(arg, True)
+
+    q = params.get("q", None)
+    p = params.get("property", None)
+    
+    if q is None:
+        q = sys.argv[1]
+    
+    if p is not None:
+        setprop = 'libreTranslate_' + p
+    else:
+        setprop = 'libreTranslate_text'
+    
+    win.setProperty(setprop, '')
+    
+    
     addon = xbmcaddon.Addon("script.LibreTranslate")
     url = addon.getSetting('LibreTranslate_url')
     target = addon.getSetting('LibreTranslate_target_lang')
     api_key = addon.getSetting('LibreTranslate_api_key')
 
-    values = {'q': sys.argv[1], 'source': 'auto', 'target': target, 'format': 'text', 'api_key': api_key}
+    values = {'q': q, 'source': 'auto', 'target': target, 'format': 'text', 'api_key': api_key}
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
@@ -28,7 +56,7 @@ def main():
     except Exception as e:
         translated_text = str(e)
 
-    win.setProperty("libreTranslate_text", translated_text)
+    win.setProperty(setprop, translated_text)
     return ''
 
 
